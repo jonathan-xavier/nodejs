44fd06aee14bd8e72449eec603814ae5133fe2d0
jonathan xavier <jonathanxavier556@gmail.com>
2025-12-02 23:28:11 -0300

    conhecendo o setHeader

--- 01-fundamentos-node/src/server.js
+++ 01-fundamentos-node/src/server.js
@@ -1,14 +1,22 @@
 import http from 'node:http'
 
+const users = []
 
 const server = http.createServer((req, res) => {
     const {method, url} = req
     if (method === "GET" && url === "/users"){
         
-        return res.end("Listagem de usuarios")
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if(method === "POST" && url === "/users"){
+        users.push({
+            id: 1,
+            name: "jhon doe",
+            email: "jhondoe@gmail.com"
+        })
         return res.end("Criacao de usuarios")
     }
     return res.end("hello")
