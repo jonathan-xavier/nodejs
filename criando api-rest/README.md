a1a2b375af4abe2bf437573fa8dc736501c092f3
jonathan xavier <jonathanxavier556@gmail.com>
2026-01-04 23:03:51 -0300

    criando o backend de teste utilizando zod

---
+++ criando api-rest/README.md
@@ -0,0 +1,14 @@
+RF - requisito funcional
+- [ ] O Usuário deve poder criar uma nova transacao
+- [ ] O usuário deve poder obter um resumo da sua conta.
+- [ ] O usuário deve poder visualizar uma transacao unica.
+- [ ] O usuario deve poder listar todas as transacoes que ja ocorreram
+
+RN - regra de negocio
+
+- [ ] A transacao pode ser do tipo crédito que somará ao valor total,
+ ou ira subtrari o debito
+- [ ] Deve ser possivel identificar o usuario entre as requisições.
+- [ ] O usuario so pode visualizer transacao que ele criou
+
+RNF - requisito nao funcional
---
+++ criando api-rest/src/server.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  //   const transaction = await knex('transactions')
+  //     .insert({
+  //       id: randomUUID(),
+  //       title: 'Transação de teste',
+  //       amount: 1000,
+  //     })
+  //     .returning('*')
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('http server running')
+  })
